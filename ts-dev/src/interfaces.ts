5b66eb3461941e33e6dec87e730ae6bef1f15f00
Komal Sathvik <mksathvik03@gmail.com>
2025-08-07 15:09:10 +0530

    new commit

--- ts-dev/src/interfaces.ts
+++ ts-dev/src/interfaces.ts
@@ -1,9 +1,9 @@
-interface animal{
-    name:string,
-    food:string,
+interface animal {
+  name: string;
+  food: string;
 }
-const dog:animal={
-    name:"dog",
-    food:"biscuits",
-}
+const dog: animal = {
+  name: "dog",
+  food: "biscuits",
+};
 console.log(dog);
